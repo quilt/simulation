694a77bd5f7142dc7be9abdb24fd9bcb23965e6f
Greg Trowbridge <gjtrowbridge@gmail.com>
2020-02-10 22:07:16 -0800

    Finish shard block func.

--- eth2/simulation/src/simulation.rs
+++ eth2/simulation/src/simulation.rs
@@ -46,7 +46,11 @@ impl<T: EthSpec> Simulation<T> {
         // For each shard, add the initial state to the shard
         for shard_state in self.store.current_beacon_state.shard_states.iter_mut() {
             // Set the initial state of the EE on each ShardState
-            shard_state.execution_environment_states.push(a.initial_state.clone());
+            shard_state.execution_environment_states.push(a.initial_state.clone()).map_err(|_| {
+                Error::MaxLengthExceeded {
+                    what: format!("number of execution environment states"),
+                }
+            })?;
 
             // Each shard should have the same # of ee states as there are EEs
             assert_eq!(shard_state.execution_environment_states.len(), self.store.current_beacon_state.execution_environments.len());
@@ -60,8 +64,8 @@ impl<T: EthSpec> Simulation<T> {
     /// Execute all transactions on the appropriate shards / EEs, return ShardBlock index
     pub fn create_shard_block(&mut self, a: args::CreateShardBlock) -> Result<ShardSlot> {
         // Get the specified ShardState (if it exists)
-        let shard_index: usize = Shard::into(a.shard);
-        let shard_state = self.store.current_beacon_state.shard_states.get(shard_index).ok_or(Error::OutOfBounds {
+        let shard_index: usize = a.shard.into();
+        let mut shard_state = self.store.current_beacon_state.shard_states.get_mut(shard_index).ok_or(Error::OutOfBounds {
             what: WhatBound::Shard,
             index: shard_index,
         })?;
@@ -79,7 +83,7 @@ impl<T: EthSpec> Simulation<T> {
         // Execute transactions and update shard state for all transactions
         for transaction in shard_block.transactions.iter() {
             // Get the specified EE (if it exists)
-            let ee_index: usize = EEIndex::into(transaction.ee_index);
+            let ee_index: usize = transaction.ee_index.into();
             let execution_environment = self.store.current_beacon_state.execution_environments.get(ee_index).ok_or(Error::OutOfBounds {
                 what: WhatBound::ExecutionEnvironment,
                 index: ee_index,
@@ -90,36 +94,29 @@ impl<T: EthSpec> Simulation<T> {
                 what: WhatBound::ExecutionEnvironmentState,
                 index: ee_index,
             })?;
-            let wasm_code = &execution_environment.wasm_code;
 
             // Create a new runtime with the EE code, transaction data, and pre state root
+            let wasm_code: Vec<u8> = execution_environment.wasm_code.clone().into();
+            let data: Vec<u8> = transaction.data.clone().into();
+            let pre_state: [u8; 32] = pre_state.clone().into();
+            let mut runtime = RootRuntime::new(&wasm_code, &data, pre_state);
+            let post_root = runtime.execute();
+            drop(runtime);
 
+            // Update shard state with new root
+            shard_state.execution_environment_states[ee_index] = Root::from(post_root);
 
-
-//            let mut runtime = Roo
-//            let data = base64::decode(&transaction.base64_encoded_data).context(Decode)?;
-//            let mut runtime = RootRuntime::new(&code, &data, pre_state.data);
-//            let post_root = runtime.execute();
-//            drop(runtime);
-//
-//            shard_chain.execution_environment_state.insert(
-//                EeIndex(transaction.ee_index),
-//                ExecutionEnvironmentState { data: post_root },
-//            );
-//
-//            transactions.push(ShardTransaction {
-//                data,
-//                ee_index: EeIndex(transaction.ee_index),
-//            });
         }
 
-        Ok(ShardSlot::new(0))
+        // Add shard block to store for later access
+        let shard_blocks_for_shard = self.store.shard_blocks_by_shard.get_mut(&a.shard).ok_or(Error::OutOfBounds {
+            index: shard_index,
+            what: WhatBound::Shard,
+        })?;
+        shard_blocks_for_shard.push(shard_block);
 
-        // Update shard state for all transactions
-        // Store transactions
-        // TODO: Make this return error if shard index is out of bounds
-//        self.store.shard_blocks_by_shard[a.shard].push(shard_block);
-
+        // Return the slot of the newly added shard block
+        Ok(ShardSlot::new((shard_blocks_for_shard.len() -1 ) as u64))
     }
 
     /// Get an EE that was previously added
